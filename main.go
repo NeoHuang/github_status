8566ff161ab3440a26f1d788e3ebdc06b513ad8f
neohuang <superneohuang@gmail.com>
2016-05-24 10:44:45 +0200

    Update usage description

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ const (
 
 OPTIONS:
     -h               Display usage
-    --high           high frequency ping interval in second. is used when github is down. default is 5 seconds
-    --low            low frequency ping interval in second. is used when github is normal. default is 60 seconds
+    --high           high frequency ping interval (example "1s" "5m" "1.5h"). is used when github is down. default is 5 seconds
+    --low            low frequency ping interval (example "1s" "5m" "1.5h"). is used when github is normal. default is 1 minute
     --channel        slack channel you wanna sent to.
 
 get github status by pinging https://status.github.com/api/status.json. Send notification to slack channel when status changed
@@ -33,7 +33,7 @@ slack team is required to set as Environment variable "SLACK_TEAM"
 slack token is required to set as Environment variable "SLACK_TOKEN"
 
 Example:
-	SLACK_TEAM=myteam SLACK_TOKEN=123456 github_status --high 2 --low 60 --channel github
+	SLACK_TEAM=myteam SLACK_TOKEN=123456 github_status --high 2s --low 1m --channel github
 `
 )
 
